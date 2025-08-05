30ee6ad02768d9c772b110437ef7c215266b6110
komikndr <mickolesmana@gmail.com>
2025-08-05 18:08:34 +0700

    adding distributed worker

--- src/raylight/nodes.py
+++ src/raylight/nodes.py
@@ -1,9 +1,12 @@
 import types
 import sys
+
 import ray
 import torch
+import torch.distributed as dist
 import comfy
 import folder_paths
+import time
 
 from comfy import sd, sample, utils
 
@@ -62,10 +65,45 @@ def is_use_fsdp() -> bool:
     return _USE_FSDP
 
 
+class Timer:
+    def __init__(self):
+        self.times = {}  # Dictionary to store times per stage
+
+    def __call__(self, name):
+        print(f"Timing {name}")
+        return self.TimerContextManager(self, name)
+
+    def print_stats(self):
+        total_time = sum(self.times.values())
+        # Print table header
+        print("{:<20} {:>10} {:>10}".format("Stage", "Time(s)", "Percent"))
+        for name, t in self.times.items():
+            percent = (t / total_time) * 100 if total_time > 0 else 0
+            print("{:<20} {:>10.2f} {:>9.2f}%".format(name, t, percent))
+
+    class TimerContextManager:
+        def __init__(self, outer, name):
+            self.outer = outer  # Reference to the Timer instance
+            self.name = name
+            self.start_time = None
+
+        def __enter__(self):
+            self.start_time = time.perf_counter()
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            end_time = time.perf_counter()
+            elapsed = end_time - self.start_time
+            self.outer.times[self.name] = self.outer.times.get(self.name, 0) + elapsed
+
+
+
 class RayWorker:
 
     master_address = None
     master_port = None
+    use_usp = False
+
 
     def __init__(self, local_rank):
         self.model = None
@@ -81,6 +119,15 @@ class RayWorker:
         self.ulysses_degree = None
         self.ring_degree = None
         self.cfg_parallel = None
+
+        t = Timer()
+        with t("init_process_group"):
+            dist.init_process_group(
+                "nccl",
+                rank=local_rank,
+                world_size=RayWorker.world_size,
+                device_id=self.device_id,  # force non-lazy init
+            )
 
     """
     Just Placeholder for now, since without USP it is just
