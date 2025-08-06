5755758879fad1659dbe6cbd38d79d0eaab97504
komikndr <mickolesmana@gmail.com>
2025-08-06 10:17:41 +0700

    Fix rope embedding to be able to use USPs

--- src/raylight/wan/distributed/xdit_context_parallel.py
+++ src/raylight/wan/distributed/xdit_context_parallel.py
@@ -7,8 +7,6 @@ from xfuser.core.distributed import (
 )
 from xfuser.core.long_ctx_attention import xFuserLongContextAttention
 
-from comfy.ldm.flux.math import apply_rope
-
 
 def sinusoidal_embedding_1d(dim, position):
     # preprocess
@@ -18,9 +16,62 @@ def sinusoidal_embedding_1d(dim, position):
 
     # calculation
     sinusoid = torch.outer(
-        position, torch.pow(10000, -torch.arange(half).to(position).div(half)))
+        position, torch.pow(10000, -torch.arange(half).to(position).div(half))
+    )
     x = torch.cat([torch.cos(sinusoid), torch.sin(sinusoid)], dim=1)
     return x
+
+
+def pad_freqs(original_tensor, target_len):
+    seq_len, s1, s2 = original_tensor.shape
+    pad_size = target_len - seq_len
+    padding_tensor = torch.ones(
+        pad_size, s1, s2, dtype=original_tensor.dtype, device=original_tensor.device
+    )
+    padded_tensor = torch.cat([original_tensor, padding_tensor], dim=0)
+    return padded_tensor
+
+
+def rope_apply(x, grid_sizes, freqs):
+    """
+    x:          [B, L, N, C].
+    grid_sizes: [B, 3].
+    freqs:      [M, C // 2].
+    """
+    s, n, c = x.size(1), x.size(2), x.size(3) // 2
+    # split freqs
+    freqs = freqs.split([c - 2 * (c // 3), c // 3, c // 3], dim=1)
+
+    # loop over samples
+    output = []
+    for i, (f, h, w) in enumerate(grid_sizes.tolist()):
+        seq_len = f * h * w
+
+        # precompute multipliers
+        x_i = torch.view_as_complex(x[i, :s].to(torch.float64).reshape(s, n, -1, 2))
+        freqs_i = torch.cat(
+            [
+                freqs[0][:f].view(f, 1, 1, -1).expand(f, h, w, -1),
+                freqs[1][:h].view(1, h, 1, -1).expand(f, h, w, -1),
+                freqs[2][:w].view(1, 1, w, -1).expand(f, h, w, -1),
+            ],
+            dim=-1,
+        ).reshape(seq_len, 1, -1)
+
+        # apply rotary embedding
+        sp_size = get_sequence_parallel_world_size()
+        sp_rank = get_sequence_parallel_rank()
+        freqs_i = pad_freqs(freqs_i, s * sp_size)
+        s_per_rank = s
+        freqs_i_rank = freqs_i[
+            (sp_rank * s_per_rank) : ((sp_rank + 1) * s_per_rank), :, :
+        ]
+        x_i = torch.view_as_real(x_i * freqs_i_rank).flatten(2)
+        x_i = torch.cat([x_i, x[i, s:]])
+
+        # append to collection
+        output.append(x_i)
+    return torch.stack(output).float()
 
 
 def usp_dit_forward(
@@ -61,7 +112,8 @@ def usp_dit_forward(
 
     # time embeddings
     e = self.time_embedding(
-        sinusoidal_embedding_1d(self.freq_dim, t).to(dtype=x[0].dtype))
+        sinusoidal_embedding_1d(self.freq_dim, t).to(dtype=x[0].dtype)
+    )
     e0 = self.time_projection(e).unflatten(1, (6, self.dim))
 
     # context
@@ -75,22 +127,35 @@ def usp_dit_forward(
         context_img_len = clip_fea.shape[-2]
 
     # Context Parallel
-    x = torch.chunk(
-        x, get_sequence_parallel_world_size(),
-        dim=1)[get_sequence_parallel_rank()]
+    x = torch.chunk(x, get_sequence_parallel_world_size(), dim=1)[
+        get_sequence_parallel_rank()
+    ]
 
     patches_replace = transformer_options.get("patches_replace", {})
     blocks_replace = patches_replace.get("dit", {})
     for i, block in enumerate(self.blocks):
         if ("double_block", i) in blocks_replace:
+
             def block_wrap(args):
                 out = {}
-                out["img"] = block(args["img"], context=args["txt"], e=args["vec"], freqs=args["pe"], context_img_len=context_img_len)
+                out["img"] = block(
+                    args["img"],
+                    context=args["txt"],
+                    e=args["vec"],
+                    freqs=args["pe"],
+                    context_img_len=context_img_len,
+                )
                 return out
-            out = blocks_replace[("double_block", i)]({"img": x, "txt": context, "vec": e0, "pe": freqs}, {"original_block": block_wrap})
+
+            out = blocks_replace[("double_block", i)](
+                {"img": x, "txt": context, "vec": e0, "pe": freqs},
+                {"original_block": block_wrap},
+            )
             x = out["img"]
         else:
-            x = block(x, e=e0, freqs=freqs, context=context, context_img_len=context_img_len)
+            x = block(
+                x, e=e0, freqs=freqs, context=context, context_img_len=context_img_len
+            )
 
     # head
     x = self.head(x, e)
@@ -119,14 +184,10 @@ def usp_attn_forward(self, x, freqs):
         return q, k, v
 
     q, k, v = qkv_fn(x)
-    q, k = apply_rope(q, k, freqs)
+    q, k = rope_apply(q, k, freqs)
 
     x = xFuserLongContextAttention()(
-        None,
-        query=q,
-        key=k,
-        value=v,
-        window_size=self.window_size
+        None, query=q, key=k, value=v, window_size=self.window_size
     )
 
     x = x.flatten(2)
