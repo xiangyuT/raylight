27c5f6320b9af6fbf516ba43a5d41041fd98b5ae
komikndr <mickolesmana@gmail.com>
2025-08-22 14:54:52 +0700

    Hotfix, fsdp module check

--- src/raylight/hunyuan_video/distributed/fsdp.py
+++ src/raylight/hunyuan_video/distributed/fsdp.py
@@ -1,41 +1,44 @@
-# For FSDP2
 from torch.distributed.fsdp import fully_shard, MixedPrecisionPolicy
-
+from torch.distributed.fsdp import FSDPModule
 
 def shard_model_fsdp2(model, device_to):
     diffusion_model = model.diffusion_model
 
-    # Shard only the blocks, since other modules have different dtype
-    # Collect params we want to ignore (everything except blocks)
-    ignored_params = set()
-    for name, param in diffusion_model.named_parameters():
-        if (not name.startswith("single_blocks.")) and (not name.startswith("double_blocks.")):
-            ignored_params.add(param)
+    # Collect params we want to ignore (everything except single_blocks + double_blocks)
+    if not isinstance(diffusion_model, FSDPModule):
+        ignored_params = set()
+        for name, param in diffusion_model.named_parameters():
+            if (not name.startswith("single_blocks.")) and (not name.startswith("double_blocks.")):
+                ignored_params.add(param)
 
-    # And also blocks is the most compute heavy part
-    diffusion_model.single_blocks = diffusion_model.single_blocks.to("cpu")
-    diffusion_model.double_blocks = diffusion_model.double_blocks.to("cpu")
-    for i, block in enumerate(diffusion_model.single_blocks):
-        if "FSDP" not in block.__class__.__name__:
+        # Move compute-heavy blocks to CPU before sharding
+        diffusion_model.single_blocks = diffusion_model.single_blocks.to("cpu")
+        diffusion_model.double_blocks = diffusion_model.double_blocks.to("cpu")
+
+        # Shard single_blocks
+        for i, block in enumerate(diffusion_model.single_blocks):
             diffusion_model.single_blocks[i] = fully_shard(
                 module=block,
                 mp_policy=MixedPrecisionPolicy(),
                 reshard_after_forward=True,
             )
-    diffusion_model.single_blocks = diffusion_model.single_blocks.to(device_to)
+        diffusion_model.single_blocks = diffusion_model.single_blocks.to(device_to)
 
-    for i, block in enumerate(diffusion_model.double_blocks):
-        if "FSDP" not in block.__class__.__name__:
+        # Shard double_blocks
+        for i, block in enumerate(diffusion_model.double_blocks):
             diffusion_model.double_blocks[i] = fully_shard(
                 module=block,
                 mp_policy=MixedPrecisionPolicy(),
                 reshard_after_forward=True,
             )
-    diffusion_model.double_blocks = diffusion_model.double_blocks.to(device_to)
+        diffusion_model.double_blocks = diffusion_model.double_blocks.to(device_to)
 
-    # Model root wrap with ignored params
-    fully_shard(diffusion_model, ignored_params=ignored_params)
-    model.diffusion_model = diffusion_model
+        # Root wrap with ignored params
+        fully_shard(diffusion_model, ignored_params=ignored_params)
+        model.diffusion_model = diffusion_model
 
-    print("SHARD COMPLETE")
-    return model
+        print("SHARD COMPLETE")
+        return model
+    else:
+        print("FSDP Already applied, skipping")
+        return model
--- src/raylight/wan/distributed/fsdp.py
+++ src/raylight/wan/distributed/fsdp.py
@@ -1,4 +1,5 @@
 from torch.distributed.fsdp import fully_shard, MixedPrecisionPolicy
+from torch.distributed.fsdp import FSDPModule
 
 
 def shard_model_fsdp2(model, device_to):
@@ -6,25 +7,28 @@ def shard_model_fsdp2(model, device_to):
 
     # Shard only the blocks, since other modules have different dtype
     # Collect params we want to ignore (everything except blocks)
-    ignored_params = set()
-    for name, param in diffusion_model.named_parameters():
-        if not name.startswith("blocks."):
-            ignored_params.add(param)
+    if not isinstance(diffusion_model, FSDPModule):
+        ignored_params = set()
+        for name, param in diffusion_model.named_parameters():
+            if not name.startswith("blocks."):
+                ignored_params.add(param)
 
-    # And also blocks is the most compute heavy part
-    diffusion_model.blocks = diffusion_model.blocks.to("cpu")
-    for i, block in enumerate(diffusion_model.blocks):
-        if "FSDP" not in block.__class__.__name__:
+        # And also blocks is the most compute heavy part
+        diffusion_model.blocks = diffusion_model.blocks.to("cpu")
+        for i, block in enumerate(diffusion_model.blocks):
             diffusion_model.blocks[i] = fully_shard(
                 module=block,
                 mp_policy=MixedPrecisionPolicy(),
                 reshard_after_forward=True,
             )
-    diffusion_model.blocks = diffusion_model.blocks.to(device_to)
+        diffusion_model.blocks = diffusion_model.blocks.to(device_to)
 
-    # Model root wrap with ignored params
-    fully_shard(diffusion_model, ignored_params=ignored_params)
-    model.diffusion_model = diffusion_model
+        # Model root wrap with ignored params
+        fully_shard(diffusion_model, ignored_params=ignored_params)
+        model.diffusion_model = diffusion_model
 
-    print("SHARD COMPLETE")
-    return model
+        print("SHARD COMPLETE")
+        return model
+    else:
+        print("FSDP Already applied, skipping")
+        return model
