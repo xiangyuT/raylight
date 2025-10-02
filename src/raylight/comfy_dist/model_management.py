4d4af7609e3e9429a5f5cc554e77edaef7118632
komikndr <mickolesmana@gmail.com>
2025-10-02 21:55:44 +0700

    remove debug line

--- src/raylight/comfy_dist/model_management.py
+++ src/raylight/comfy_dist/model_management.py
@@ -7,7 +7,6 @@ import comfy.model_management as mm
 
 
 def cleanup_models_gc():
-    print("Internal to ray")
     do_gc = False
     for i in range(len(mm.current_loaded_models)):
         cur = mm.current_loaded_models[i]
@@ -25,5 +24,6 @@ def cleanup_models_gc():
             if cur.is_dead():
                 if isinstance(cur.real_model().diffusion_model, FSDPModule):
                     mm.current_loaded_models.pop(i)
+                    logging.info("FSDP wrapped module detected with model {}".format(cur.real_model().__class__.__name__))
                 else:
                     logging.warning("WARNING, memory leak with model {}. Please make sure it is not being referenced from somewhere.".format(cur.real_model().__class__.__name__))
