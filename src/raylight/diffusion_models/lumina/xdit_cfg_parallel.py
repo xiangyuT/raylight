022d7c99e5f09d2e225126d08d5fca2b5875e1a9
komikndr <mickolesmana@gmail.com>
2025-11-20 14:52:24 +0700

    Testing Lumina

---
+++ src/raylight/diffusion_models/lumina/xdit_cfg_parallel.py
@@ -0,0 +1,27 @@
+import torch
+from xfuser.core.distributed import (
+    get_classifier_free_guidance_rank,
+    get_classifier_free_guidance_world_size,
+    get_cfg_group,
+)
+
+
+def cfg_parallel_forward_wrapper(executor, *args, **kwargs):
+    cfg_rank = get_classifier_free_guidance_rank()
+    cfg_world_size = get_classifier_free_guidance_world_size()
+
+    x, timesteps, context, num_tokens, attention_mask = args
+
+    x = torch.chunk(x, cfg_world_size, dim=0)[cfg_rank]
+    timesteps = torch.chunk(timesteps, cfg_world_size, dim=0)[cfg_rank]
+    context = torch.chunk(context, cfg_world_size, dim=0)[cfg_rank]
+
+    if num_tokens is not None:
+        num_tokens = torch.chunk(num_tokens, cfg_world_size, dim=0)[cfg_rank]
+
+    if attention_mask is not None:
+        attention_mask = torch.chunk(attention_mask, cfg_world_size, dim=0)[cfg_rank]
+
+    result = executor(x, timesteps, context, num_tokens, attention_mask, **kwargs)
+    result = get_cfg_group().all_gather(result, dim=0)
+    return result
